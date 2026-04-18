c491de059b2cf76432cb56fce8b5b75135cc4f3a
loecedas <loecedas@gmail.com>
2026-04-18 11:41:04 +0800

    feat: add multi-platform support (Vercel & Netlify) and fix Netlify 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
+
+const adapter = process.env.NETLIFY ? netlify() : vercel();
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: adapter,
   image: {
     service: passthroughImageService(),
   },
